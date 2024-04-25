4139c678a2b06e8456e37499d3b98ea15c678ddb
ilhamhadisaputro <ilhamhadisaputro28@gmail.com>
2024-04-26 00:51:45 +0700

    Add admin and user login v0.2

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class LoginController extends Controller
+{
+    public function login() {
+        if (Auth::check()) {
+            return redirect('home');
+        } else {
+            return view ('login');
+        }
+    }
+
+    public function actionLogin(Request $request) {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+        if (AUTH::attempt($data)) {
+            return redirect('home');
+        }else {
+            Session::flash('error', 'email atau pasword salah');
+            return redirect('/');
+        }
+    }
+
+    public function actionLogout() {
+        Auth::logout();
+        return redirect('/');
+    }
+}
